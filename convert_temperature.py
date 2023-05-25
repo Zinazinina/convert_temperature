a31230cbe6eb31ab8050fc0799072d8b135e3af1
Zixuan <serein_liu@outlook.com>
2023-05-25 16:26:16 +0200

    Addition.

--- convert_temperature.py
+++ convert_temperature.py
@@ -7,6 +7,8 @@ def fahrenheit_to_celsius(temperature_fahrenheit):
 
 	Returns:
 	temperature_celsius
+
+	Addition
 	"""
 	temperature_celsius = (temperature_fahrenheit-32.0)*5.0/9.0
 	return temperature_celsius
