89fc26749f8d0d9d78ec9e90266077ae7fcb2380
Joshua Spaander <josh.spaander@hotmail.com>
2023-05-25 14:22:44 +0200

    Function documentation added in form of doc string.

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,3 +1,12 @@
 def fahrenheit_to_celsius(temperature_fahrenheit):
+	"""
+	Converts temperature in fahrenheit to celsius
+
+	Parameters:
+	temperature_fahrenheit
+
+	Returns:
+	temperature_celsius
+	"""
 	temperature_celsius = (temperature_fahrenheit-32.0)*5.0/9.0
 	return temperature_celsius
